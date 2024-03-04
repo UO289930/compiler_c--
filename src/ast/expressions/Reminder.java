227bf95535e67bdb3697613018880f4aa915c7c2
Sergio Truébano <uo289930@uniovi.es>
2024-03-04 18:18:15 +0100

    Reminder ToString change

--- src/ast/expressions/Reminder.java
+++ src/ast/expressions/Reminder.java
@@ -1,7 +1,30 @@
 package ast.expressions;
 
-public class Reminder extends AbstractOperatorExpression{
-    public Reminder(int line, int column, String operator, Expression operand1, Expression operand2) {
-        super(line, column, operator, operand1, operand2);
+import ast.AbstractASTNode;
+
+public class Reminder extends AbstractASTNode implements Expression{
+
+    private final Expression operand1;
+
+    private final Expression operand2;
+
+
+    public Reminder(int line, int column, Expression operand1, Expression operand2) {
+        super(line, column);
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public Expression getOperand1() {
+        return operand1;
+    }
+
+    public Expression getOperand2() {
+        return operand2;
+    }
+
+    @Override
+    public String toString() {
+        return operand1 + "%" + operand2;
     }
 }
